024339addc2a65f848f2cad63fcbef4b60b9c960
jhonathanalencar <jhonathan.alencarfs@gmail.com>
2024-04-21 11:04:05 -0300

    feat: testing async component

--- src/components/card/card.component.test.tsx
+++ src/components/card/card.component.test.tsx
@@ -1,4 +1,5 @@
 import { render, screen, within } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
 import { Card } from "./card.component";
 
@@ -17,6 +18,30 @@ describe("<Card>", () => {
       // expect(card).toBeInTheDocument();
       expect(divElement).toBeVisible();
       expect(title).toBeVisible();
+    });
+
+    it("should render async component", async () => {
+      render(<Card />);
+
+      const paragraph = await screen.findByRole("paragraph");
+
+      expect(paragraph).toBeVisible();
+    });
+
+    it("should open component when button was clicked", async () => {
+      render(<Card />);
+
+      const button = screen.getByRole("button", { name: /open/i });
+
+      let paragraph = screen.queryByRole("paragraph");
+
+      expect(paragraph).not.toBeInTheDocument();
+
+      await userEvent.click(button);
+
+      paragraph = screen.getByRole("paragraph");
+
+      expect(paragraph).toBeVisible();
     });
   });
 });
--- src/components/card/card.component.tsx
+++ src/components/card/card.component.tsx
@@ -1,7 +1,35 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
 export function Card() {
+  const [isVisible, setIsVisible] = useState(false);
+
+  function handleOpen() {
+    setIsVisible(true);
+  }
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setIsVisible(true);
+    }, 300);
+
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, []);
+
   return (
     <div data-testid="card" role="group">
       <h2>Card</h2>
+
+      <button onClick={handleOpen}>Open</button>
+
+      {isVisible ? (
+        <div>
+          <p role="paragraph">Content</p>
+        </div>
+      ) : null}
     </div>
   );
 }
